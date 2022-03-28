586d8739779a787df8ce6ab0cbc7730571ea29e5
................ <vishalbariya1099@gmail.com>
2022-03-29 00:42:39 +0530

    All Layers Added

---
+++ scholarshipbackend/src/main/java/com/lti/app/scholarshipbackend/ScholarshipbackendApplication.java
@@ -0,0 +1,13 @@
+package com.lti.app.scholarshipbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScholarshipbackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ScholarshipbackendApplication.class, args);
+	}
+
+}
